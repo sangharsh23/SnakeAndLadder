3901bfb5b951f5d9608c14208eb61ec56fe6bfc5
sangharsh23 <sangharsh355@gmail.com>
2021-09-17 22:30:23 +0530

    Added: program total dice roll count for play

--- src/main/java/com/bridgelabz/snake/and/ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake/and/ladder/SnakeAndLadder.java
@@ -1,7 +1,49 @@
 package com.bridgelabz.snake.and.ladder;
 
 public class SnakeAndLadder {
+    public static final int WINNING_POSITION = 100;
+    public static final int IS_LADDER = 1;
+    public static final int IS_SNAKE = 0;
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Program");
+        int startPosition = 0;
+        System.out.println("Player staring position is : " + startPosition);
+
+
+        int position = 0;
+        int diceCount = 0;
+
+        while (position < WINNING_POSITION) {
+            ++diceCount;
+
+            int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            System.out.println("diceNumber is : " + diceNumber);
+
+
+            int option = (int) Math.floor(Math.random() * 10) % 2;
+            System.out.println("selected player option : " + option);
+
+            if (option == IS_SNAKE) {
+                System.out.println("Snake for the Player");
+                if(position - diceNumber > 0){
+                    position = position - diceNumber;
+                    System.out.println("Position for the Player after the snake : " + position);
+                }
+            }
+            else if (option == IS_LADDER) {
+                System.out.println("Ladder for the Player");
+                if (position + diceNumber <= 100) {
+                    position = position + diceNumber;
+                    System.out.println("Position for the Player after the snake : " + position);
+                }
+                System.out.println("Total dice roll count is " + diceCount);
+            }
+
+        }
+
     }
 }
+
+
