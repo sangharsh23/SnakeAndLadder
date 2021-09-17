7e89ff997cf96602c5a04ffb1806c5bf3d47ca88
sangharsh23 <sangharsh355@gmail.com>
2021-09-17 21:01:35 +0530

    Added: Welcome msg

---
+++ src/main/java/com/bridgelabz/snake/and/ladder/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.snake.and.ladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder Program");
+    }
+}
