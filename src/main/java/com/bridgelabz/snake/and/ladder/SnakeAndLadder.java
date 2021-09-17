814ebd52bdef4d18a635df56310110ba1f77e213
sangharsh23 <sangharsh355@gmail.com>
2021-09-17 21:12:02 +0530

    Added: Program dice roll number

--- src/main/java/com/bridgelabz/snake/and/ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake/and/ladder/SnakeAndLadder.java
@@ -3,5 +3,8 @@ package com.bridgelabz.snake.and.ladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Program");
+
+        int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println("diceNumber is : " + diceNumber);
     }
 }
