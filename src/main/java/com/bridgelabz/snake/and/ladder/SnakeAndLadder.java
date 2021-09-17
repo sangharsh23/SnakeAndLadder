1a9c66b64f39d9f250237dc7bed21dbfd35eeb51
sangharsh23 <sangharsh355@gmail.com>
2021-09-17 21:07:00 +0530

    Added: program player start position

--- src/main/java/com/bridgelabz/snake/and/ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake/and/ladder/SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.bridgelabz.snake.and.ladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Program");
+        int startPosition = 0;
+        System.out.println("Player starting position  : " + startPosition);
     }
 }
